0a912426c24db361d7d49067fabfeee3ea54953a
Дмитрий <demosne@gmail.com>
2021-05-27 14:26:51 +0300

    html

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -16,6 +16,73 @@ export default function Form() {
             </div>
             <a href="#">Сменить статус</a>
           </section>
+
+          <section>
+            <div className="left"></div>
+            <div className="middle">Прежде чем действовать, надо понять</div>
+          </section>
+
+          <section>
+            <div className="left note">Ваш город</div>
+            <div className="middle">
+              <select>
+                <option>Красноярск</option>
+              </select>
+            </div>
+          </section>
+
+          <form>
+            <section>
+              <div className="left note">Пароль</div>
+              <div className="middle">
+                <input type="text"></input>
+              </div>
+              <div className="decor">
+                Ваш новый пароль должен содержать не менее 5 символов.
+              </div>
+            </section>
+
+            <section>
+              <div className="left note">Пароль еще раз</div>
+              <div className="middle">
+                <input type="text"></input>
+              </div>
+              <div className="decor">
+                Повторите пароль, пожалуйста, это обезопасит вас с нами на
+                случай ошибки.
+              </div>
+            </section>
+
+            <section>
+              <div className="left note">Электронная почта</div>
+              <div className="middle">
+                <input type="email"></input>
+              </div>
+              <div className="decor">
+                Можно изменить адрес, указанный при регистрации.
+              </div>
+            </section>
+
+            <section>
+              <div className="left note">Я согласен</div>
+              <div className="middle">
+                <label>
+                  <input type="checkbox"></input>
+                  принимать актуальную информацию на емейл
+                </label>
+              </div>
+            </section>
+
+            <section>
+              <div className="left"></div>
+              <div className="middle">
+                <button type="submit">Изменить</button>
+                <div className="changes">
+                  последние изменения 15 мая 2012 в 14:55:17
+                </div>
+              </div>
+            </section>
+          </form>
         </div>
       </div>
     </>
