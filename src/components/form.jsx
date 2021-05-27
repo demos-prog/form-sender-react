76024adfa92fe0eda1e506a79b7a166a31beabab
Дмитрий <demosne@gmail.com>
2021-05-27 13:43:55 +0300

    start

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -3,6 +3,21 @@ import "./null_styles.css";
 import "./form.css";
 
 export default function Form() {
-
-  return <></>;
+  return (
+    <>
+      <div className="wrapper">
+        <div className="container">
+          <section>
+            <div className="left title">
+              <strong>Здравствуйте, </strong>
+            </div>
+            <div className="middle title">
+              <strong>Человек №3452342535</strong>
+            </div>
+            <a href="#">Сменить статус</a>
+          </section>
+        </div>
+      </div>
+    </>
+  );
 }
